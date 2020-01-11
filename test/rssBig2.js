66ba3eb421bf1fcb9d39e5a384468ae9f0e4a8c0
echosoar <82163514@qq.com>
2020-01-11 15:42:37 +0800

    fix: support element name include :

---
+++ test/rssBig2.js
@@ -0,0 +1,344 @@
+const xmldata = `<?xml version="1.0" encoding="UTF-8"?>
+<rss  xmlns:atom="http://www.w3.org/2005/Atom" version="2.0"
+
+>
+    <channel>
+        <title><![CDATA[京东图书 - 什么值得买]]></title>
+        <link>http://search.smzdm.com/?c=home&#38;s=%E4%BA%AC%E4%B8%9C%E5%9B%BE%E4%B9%A6&#38;order=time&#38;v=b</link>
+        <atom:link href="http://rsshub.app/smzdm/keyword/%E4%BA%AC%E4%B8%9C%E5%9B%BE%E4%B9%A6" rel="self" type="application/rss+xml" />
+        <description><![CDATA[京东图书 - 什么值得买 - Made with love by RSSHub(https://github.com/DIYgod/RSSHub)]]></description>
+        <generator>RSSHub</generator>
+        <webMaster>i@diygod.me (DIYgod)</webMaster>
+
+
+        <language>zh-cn</language>
+
+        <lastBuildDate>Sat, 11 Jan 2020 07:30:52 GMT</lastBuildDate>
+        <ttl>50</ttl>
+
+        <item>
+            <title><![CDATA[0-2岁小鸡球球洞洞认知书（套装全4册） - 28.38元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://qny.smzdm.com/202001/11/5e197986c31326583.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 07:30:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18614292/</guid>
+            <link>https://www.smzdm.com/p/18614292/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[《培生自然拼读故事会》（含40册故事书+96张单词认读卡+1张CD等） - 106.12元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://y.zdmimg.com/201906/06/5cf8b6d4a904e9580.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 07:29:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18614284/</guid>
+            <link>https://www.smzdm.com/p/18614284/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[《它们是怎么来的》精装大开本少儿科普百科全书 - 23.4元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://y.zdmimg.com/201909/28/5d8f6fddabac5732.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 07:28:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18614261/</guid>
+            <link>https://www.smzdm.com/p/18614261/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[《维吉尼亚·李·伯顿经典绘本》（套装共8册 生命的故事、小房子等） - 59.51元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://y.zdmimg.com/201906/11/5cff0aeaef3e1183.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 07:24:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18614220/</guid>
+            <link>https://www.smzdm.com/p/18614220/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[《DK儿童百科全书》精致版 - 26.25元（需用券）]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://y.zdmimg.com/201809/04/5b8e00d1ad0548047.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 07:20:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18614175/</guid>
+            <link>https://www.smzdm.com/p/18614175/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[我的第一本艺术书（全2册） - 42.03元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://y.zdmimg.com/201911/21/5dd642b0491ad4346.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 07:09:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18614038/</guid>
+            <link>https://www.smzdm.com/p/18614038/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[《好玩的数学绘本 1～3辑》（ 共30册） - 112.5元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://y.zdmimg.com/201703/27/58d8bae2402f36944.png_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 07:08:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18614025/</guid>
+            <link>https://www.smzdm.com/p/18614025/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[多莱尔作品·北欧神话 - 25.65元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://qny.smzdm.com/202001/11/5e1973dfc67143079.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 07:06:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18613987/</guid>
+            <link>https://www.smzdm.com/p/18613987/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[漫威电影宇宙十周年海报集 - 40.01元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://qny.smzdm.com/202001/11/5e19732db07e39242.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 07:03:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18613952/</guid>
+            <link>https://www.smzdm.com/p/18613952/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[凯迪克金奖绘本：诺亚方舟 - 10.68元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://qny.smzdm.com/202001/11/5e1972b4d8bf54536.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 07:01:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18613908/</guid>
+            <link>https://www.smzdm.com/p/18613908/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[《凯迪克大奖绘本：泥巴人&赫舍尔和光明节妖精》（套装共2册） - 24.75元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://qny.smzdm.com/201902/11/5c61885fa2f775486.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 06:59:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18613882/</guid>
+            <link>https://www.smzdm.com/p/18613882/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[《DK经典科普作品：大灾难 》（精装） - 17.96元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://qny.smzdm.com/201906/20/5d0b2a0cee4477180.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 06:55:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18613815/</guid>
+            <link>https://www.smzdm.com/p/18613815/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[《凯迪克银奖绘本：太阳和月亮为什么住在天上》 - 9.33元（需用券）]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://qny.smzdm.com/201909/27/5d8dbeb68d8b26404.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 06:49:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18613706/</guid>
+            <link>https://www.smzdm.com/p/18613706/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[《杨红樱淘气包马小跳系列典藏第1辑共10册》 - 53.32元（需用券）]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://y.zdmimg.com/201912/15/5df555f0b652d9602.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 06:44:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18613619/</guid>
+            <link>https://www.smzdm.com/p/18613619/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[玛格丽特经典绘本：逃家小兔+晚安月亮+小岛（套装全3册） - 30元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://qny.smzdm.com/202001/11/5e196e605e61c2696.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 06:43:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18613591/</guid>
+            <link>https://www.smzdm.com/p/18613591/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[京东PLUS会员 : 《培生儿童英语分级阅读Level 1》（20册图画书+40张单词卡+光盘） - 低至47.47元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://y.zdmimg.com/201809/04/5b8dffbe5efb97110.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 06:41:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18613572/</guid>
+            <link>https://www.smzdm.com/p/18613572/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[《3-6岁语感启蒙欢乐英文童谣》（20册绘本+1张CD+2只指偶） - 44.81元（需用券）]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://qny.smzdm.com/201906/29/5d170f32b37b6696.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 06:40:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18613546/</guid>
+            <link>https://www.smzdm.com/p/18613546/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[小鸡球球的大成长（5册） - 49.8元（需用券）]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://y.zdmimg.com/201909/28/5d8f39e31da461445.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 06:38:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18613510/</guid>
+            <link>https://www.smzdm.com/p/18613510/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[《汪汪队立大功儿童安全救援故事书 第1+2辑》（全18册） - 61.5元]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://qny.smzdm.com/201710/31/59f837d555b728862.png_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 06:35:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18613448/</guid>
+            <link>https://www.smzdm.com/p/18613448/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+        <item>
+            <title><![CDATA[《美国国家地理儿童小百科》（套装共6册） - 37.5元（需用券）]]></title>
+            <description><![CDATA[<br>京东<br><img src="http://qny.smzdm.com/201903/23/5c95a9cf1702e8438.jpg_d200.jpg" referrerpolicy="no-referrer">]]></description>
+            <pubDate>Sat, 11 Jan 2020 06:33:00 GMT</pubDate>
+            <guid isPermaLink="false">https://www.smzdm.com/p/18613387/</guid>
+            <link>https://www.smzdm.com/p/18613387/</link>
+
+
+
+
+
+
+
+
+        </item>
+
+    </channel>
+</rss>`
+
+const Dolmx = require('../build/dolmx');
+let xml = Dolmx(xmldata);
+console.log(JSON.stringify(xml, null, '\t'));
