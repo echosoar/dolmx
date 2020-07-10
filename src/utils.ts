01d862c77cd23f4fd5899e38131703278378d1ed
echosoar <82163514@qq.com>
2020-07-10 23:57:33 +0800

    feat: format attribute use O(n)

--- src/utils.ts
+++ src/utils.ts
@@ -18,23 +18,34 @@ export const formatAttr = (attr: string) => {
   let key = '';
   let value;
   let matchKey = true;
-  let valueSplit = ' ';
+  let valueSplit = '';
   for (const char of attr) {
-    if (matchKey) {
+    if (matchKey) {// 英文、数字、减号、下划线、和英文句
       if (char === ' ') {
         continue;
-      } else if (char !== '=') {
-        key += char;
-      } else {
+      } else if (char === '=') {
+        if (!key) {
+          throw new Error('xml property name cannot is empty');
+        }
         attrObj[key] = true;
         matchKey = false;
+      } else {
+        if (!key && !/[a-z_]/i.test(char)) {
+          throw new Error('xml property name must be start with a-z or _');
+        }
+        if (!/[\w-]/i.test(char)) {
+          throw new Error('xml property name must be use a-z/0-9/-/_');
+        }
+        key += char;
       }
     } else {
       if (value === undefined) {
         if (char === '"' || char === `'`) {
           valueSplit = char;
+          value = '';
+        } else if (char !== ' ') {
+          throw new Error('xml property value must be start with \' or "');
         }
-        value = '';
       } else {
         if (char === valueSplit) {
           matchKey = true;
--- test/declear.test.ts
+++ test/declear.test.ts
@@ -7,16 +7,14 @@ describe('/test/index.test.ts', () => {
     assert(result.xml._attr.encoding === 'UTF-8');
   });
   it('declare error', async () => {
-    const result: any = dolmx(`<?xml version="1.0 encoding="UTF-8"?>`);
-    assert(result.xml._attr.version === '1.0 encoding=');
-    assert(result.xml._attr['UTF-8"'] === true);
+    try {
+      dolmx(`<?xml xx+?>`);
+    } catch (e) {
+      assert(e.message === 'xml property name must be use a-z/0-9/-/_');
+    }
   });
   it('declare property with space', async () => {
     const result: any = dolmx(`<?xml version = " 1.0 "?>`);
     assert(result.xml._attr.version === ' 1.0 ');
-  });
-  it('declare escape', async () => {
-    const result: any = dolmx(`<?xml version = " 1.0 "?>`);
-    assert(result);
   });
 });
